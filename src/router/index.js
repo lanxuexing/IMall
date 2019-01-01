8c3c4b89c7b9e7e3cdc46fb4ddf1683dd2ee401f
lanxuexing_qt <lanxuexing_qt@he.chinamobile.com>
2019-01-01 21:52:46 +0800

    router test

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '../views/goodsList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/goods/:goodsId',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
---
+++ src/views/goodsList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>这里是商品列表界面
+            <span>{{$route.params.goodsId}}</span>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
