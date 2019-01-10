a7ed9fd748a6ef0045a0a3ae8a5dc87b74192818
lanxuexing_qt <lanxuexing_qt@he.chinamobile.com>
2019-01-10 21:19:11 +0800

    server: goods sort limit

--- server/models/goods.js
+++ server/models/goods.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var productSchema = new Schema({
+const productSchema = new Schema({
     "productId": String,
     "productName": String,
     "salePrice": Number,
--- server/routes/goods.js
+++ server/routes/goods.js
@@ -1,26 +1,35 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Goods = require('../models/goods');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Goods = require('../models/goods');
 
 // 连接MongoDb数据库
 mongoose.connect('mongodb://127.0.0.1:27017/imall');
 
-mongoose.connection.on('connected', function() {
+mongoose.connection.on('connected', () => {
     console.log('Mongodb connected success.');
 });
 
-mongoose.connection.on('error', function() {
+mongoose.connection.on('error', () => {
     console.log('Mongodb connected error.');
 });
 
-mongoose.connection.on('disconnected', function() {
+mongoose.connection.on('disconnected', () => {
     console.log('Mongodb connected disconnected.');
 });
 
 
-router.get('/', function (req, res, next) {
-    Goods.find({}, function (err, doc) {
+router.get('/', (req, res, next) => {
+    const page = parseInt(req.param('page'));
+    const pageSize = parseInt(req.param('pageSize'));
+    const skip = (page - 1) * pageSize;
+    const sort = req.param('sort');
+    const params = {};
+    const goodsModel = Goods.find(params).skip(skip).limit(pageSize); // 排序并分页
+    goodsModel.sort({
+        'salePrice': sort
+    });
+    goodsModel.exec((err, doc) => {
         if (err) {
             res.json({
                 status: '1',
