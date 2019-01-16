0a329c8feb9501db1fcced7d4888b508fea3c60e
lanxuexing_qt <lanxuexing_qt@he.chinamobile.com>
2019-01-16 22:39:33 +0800

    add orderConfirm component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import GoodsList from '../views/GoodsList';
-import Cart from '../views/Cart';
-import Address from '../views/Address';
+import GoodsList from '@/views/GoodsList';
+import Cart from '@/views/Cart';
+import Address from '@/views/Address';
+import OrderConfirm from '@/views/OrderConfirm';
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/orderConfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
     }
   ]
 })
