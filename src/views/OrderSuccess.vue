ad89c325797fdc94e6e4e4170f699a20a1b7bafc
lanxuexing_qt <lanxuexing_qt@he.chinamobile.com>
2019-01-20 23:19:03 +0800

    add OrderSuccess component

---
+++ src/views/OrderSuccess.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <nav-header/>
+    <nav-bread>
+      <!-- 第一种方式 -->
+      <span>OrderSuccess</span>
+      <!-- 第二种方式 -->
+      <!-- <span slot="bread">Goods</span> -->
+    </nav-bread>
+    <div class="container">
+      <div class="page-title-normal">
+        <h2 class="page-title-h2">
+          <span>check out</span>
+        </h2>
+      </div>
+      <!-- 进度条 -->
+      <div class="check-step">
+        <ul>
+          <li class="cur">
+            <span>Confirm</span> address
+          </li>
+          <li class="cur">
+            <span>View your</span> order
+          </li>
+          <li class="cur">
+            <span>Make</span> payment
+          </li>
+          <li class="cur">
+            <span>Order</span> confirmation
+          </li>
+        </ul>
+      </div>
+
+      <div class="order-create">
+        <div class="order-create-pic">
+          <img src="/static/ok-2.png" alt>
+        </div>
+        <div class="order-create-main">
+          <h3>Congratulations!
+            <br>Your order is under processing!
+          </h3>
+          <p>
+            <span>Order ID：100000001</span>
+            <span>Order total：1000</span>
+          </p>
+          <div class="order-create-btn-wrap">
+            <div class="btn-l-wrap">
+              <a href="javascript:;" class="btn btn--m">Cart List</a>
+            </div>
+            <div class="btn-r-wrap">
+              <a href="javascript:;" class="btn btn--m">Goods List</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav-footer/>
+  </div>
+</template>
+
+<script>
+import "./../assets/css/checkout.css";
+import NavHeader from "@/components/NavHeader";
+import NavBread from "@/components/NavBread";
+import Modal from "@/components/Modal";
+import NavFooter from "@/components/NavFooter";
+import axios from "axios";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  components: {
+    NavHeader,
+    NavBread,
+    Modal,
+    NavFooter
+  },
+  methods() {}
+};
+</script>
+
+<style scoped>
+</style>
